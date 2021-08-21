048776e17dde634ada23c0bcaea9c2b70df63a5f
Sung Lee <sungw.lee1@gmail.com>
2021-08-21 13:50:50 -0400

    Add controlled form to addProject component

--- ppm-react-client/src/App.js
+++ ppm-react-client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
+          <Route exact path="/" component={Dashboard} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
         </div>
